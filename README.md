eee5cb708b7e3e49c4008af35f149913578491a5
609457773@qq.com <609457773@qq.com>
2018-08-06 17:07:35 +0800

    first com

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# ac-dnd-transfer
-拖拽穿梭组件
-sdf
+## Ac-dnd-transfer
+
+### 1. 简介
+
+> 基于tinper-bee组件库封装开发
+
+> 在 Tranfer 组件基础上添加拖拽功能
+
+
+
+
+### 2. 安装
+
+- 通过ynpm下载使用(现在只支持ynpm进行下载)
+
+`ynpm install @yonyou/ac-dnd-tranfer -S`
+
+### 3. 使用
+
+通过ES6的方式进行加载使用
+
+```js
+import AcDndTranfer from '@yonyou/ac-dnd-tranfer';//加载组件
+```
+然后在render使用的时候传入相应组件需要的参数：
+
+具体的请见 [tinper-bee/bee-transfer](https://github.com/tinper-bee/bee-transfer/blob/master/README.md)
+
---
+++ src/index.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Transfer } from 'tinper-bee';
+const propTypes = {
+    title: PropTypes.string, 
+};
+import 'tinper-bee/assets/tinper-bee.css';
+class AcDndTransfer extends Component {
+    constructor(props, context) {
+      super(props, context);
+      this.state = {
+        enterKey:'',
+        targetKeys:this.props.targetKeys||[]
+      };
+      this.dragEnter=this.dragEnter.bind(this)
+      this.dragEnd=this.dragEnd.bind(this)
+    }
+    componentDidMount() {
+     
+    }
+    dragEnter(e,key){
+      console.log('enterKey',key)
+      this.setState({
+        enterKey:key
+      })
+    }
+    dragEnd(e,key){
+      const { enterKey, targetKeys } = this.state
+      const tempTargetKeys = targetKeys.slice()
+      let dragKey = tempTargetKeys.findIndex((item)=>item===key)
+      let dragEnterKey = tempTargetKeys.findIndex((item)=>item===enterKey)
+      console.log(dragKey,dragEnterKey)
+
+      let tempObj = tempTargetKeys[dragKey]
+      tempTargetKeys[dragKey]=tempTargetKeys[dragEnterKey]
+      tempTargetKeys[dragEnterKey]=tempObj
+      this.setState({
+        targetKeys:tempTargetKeys
+      })
+      console.log('dragEnd',key);
+    }
+
+    render() {
+      const {
+        targetKeys
+      } = this.state
+      console.log(targetKeys)
+      return (
+        <Transfer 
+          {...this.props}
+          targetKeys={targetKeys}
+          render={(da)=>{
+              return (<span className="item" id={da.key} key={da.key} draggable="true" onDragEnd={(e)=>this.dragEnd(e,da.key)} onDragStart={this.dragStart} onDragEnter={(e)=>this.dragEnter(e,da.key)}>
+              {da.title}
+             </span>)
+            }
+          }
+        />
+      );
+    }
+  };
+
+AcDndTransfer.propTypes = propTypes; 
+export default AcDndTransfer;
